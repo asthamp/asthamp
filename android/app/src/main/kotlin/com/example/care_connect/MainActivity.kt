747c4737de892dda940f2ca74353404cba70f430
AsthaMP <amallapaudel@student.umgc.edu>
2025-06-28 10:57:40 -0400

    Initial commit excluding env files

--- android/app/src/main/kotlin/com/example/care_connect/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.care_connect
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()  // this for the patientcall to get video call
-
-//class MainActivity : FlutterActivity()
